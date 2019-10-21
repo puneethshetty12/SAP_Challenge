b344da6ae8dba165747dfba123a4c4bbedc1c43c
Puneeth Shetty <puneethshetty12@gmail.com>
2019-10-21 23:32:07 +0200

    create memo id, create proximity alert

--- app/src/main/java/com/coresystems/codelab/view/create/CreateMemo.kt
+++ app/src/main/java/com/coresystems/codelab/view/create/CreateMemo.kt
@@ -1,11 +1,14 @@
 package com.coresystems.codelab.view.create
 
+import android.app.PendingIntent
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import com.coresystems.codelab.R
+import com.coresystems.codelab.alert.NotificationReceiver
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -13,9 +16,7 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.content_create_memo.*
-
-
-
+import java.util.*
 
 
 /**
@@ -55,9 +56,11 @@ class CreateMemo : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapClick
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_save -> {
+                println("ID :"+createMemoId())
                 model.updateMemo(memo_title.text.toString(), memo_description.text.toString(), reminderLatitude, reminderLongitude)
                 if (model.isMemoValid()) {
                     model.saveMemo()
+                    //createProximityAlert()
                     finish()
                 } else {
                     memo_title_container.error = model.getTitleError(this)
@@ -89,6 +92,22 @@ class CreateMemo : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapClick
         memo_location.setText("Location added")
     }
 
+    private fun createMemoId(): Int{
+        val calender = Calendar.getInstance()
+        val hour = calender.get(Calendar.HOUR_OF_DAY)
+        val min = calender.get(Calendar.MINUTE)
+        val sec = calender.get(Calendar.SECOND)
+        val milliSec = calender.get(Calendar.MILLISECOND)
+        return Integer.parseInt(""+hour+""+min+""+sec+""+milliSec)
+    }
+
+    /**
+     * Creates a pendingIntent and adds it to proximityAlert
+     */
+    private fun createProximityAlert(){
+        val intent = Intent(applicationContext, NotificationReceiver::class.java)
+        var pendingIntent = PendingIntent.getBroadcast(applicationContext,0,intent, 0)
+    }
     override fun onResume() {
         super.onResume()
         mapView.onResume()
