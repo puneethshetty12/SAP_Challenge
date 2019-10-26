7e15710a59eed6a07877638617f25b9bad18897a
Puneeth Shetty <puneethshetty12@gmail.com>
2019-10-26 14:27:34 +0200

    LocationUtil fix and tested

--- app/src/main/java/com/coresystems/codelab/alert/LocationUtil.kt
+++ app/src/main/java/com/coresystems/codelab/alert/LocationUtil.kt
@@ -13,13 +13,7 @@ object LocationUtil {
             var location = geocoder.getFromLocation(lat,lon, 1)
             var obj: Address = location.get(0)
             var add = obj.getAddressLine(0)
-            add = add + "\n" + obj.countryName
-            //add = add + "\n" + obj.countryCode
-            //add = add + "\n" + obj.adminArea
-            add = add + "\n" + obj.postalCode
-            add = add + "\n" + obj.subAdminArea
-            add = add + "\n" + obj.locality
-            add = add + "\n" + obj.subThoroughfare
+            println(add)
             return add
         } catch (e: Exception){
             println("Error getting location name -"+e)
