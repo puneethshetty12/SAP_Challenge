e3df8184c5910cf9c4a3c83a0013156c9f3598e9
Puneeth Shetty <puneethshetty12@gmail.com>
2019-10-27 19:38:39 +0100

    saved

--- app/src/main/java/com/coresystems/codelab/alert/NotificationReceiver.kt
+++ app/src/main/java/com/coresystems/codelab/alert/NotificationReceiver.kt
@@ -3,18 +3,10 @@ package com.coresystems.codelab.alert
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.location.LocationManager
 
 class NotificationReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent?) {
-        val key =  LocationManager.KEY_PROXIMITY_ENTERING;
-        val entering = intent!!.getBooleanExtra(key, false)
-        if(entering){
-            println("Entering")
-        } else {
-            println("Exiting")
-        }
         val title = intent!!.getStringExtra("MESSAGE_TITLE")
         val desc = intent.getStringExtra("MESSAGE_DESCRIPTION")
         NotificationHelper.createNotification(context, title, desc)
